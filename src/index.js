5f1dad1ee267c3fa5cae4c7ed30fdc1738987c5c
Stanley  Dsouza <stanley.dsouza@schbang.com>
2020-08-07 15:52:01 +0530

    added router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
-
-ReactDom.render(<App />, document.getElementById('root'));
+const routing = (
+    <HashRouter>
+      <Route path='/' component={ App } exact />
+   </HashRouter>
+  )
+// ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(routing,document.getElementById('root'));
